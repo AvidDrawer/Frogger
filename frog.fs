ff1637e65cc00d1542d704313766566b89eb69d8
prash <enprush@gmail.com>
2020-04-07 08:26:01 +0530

    Revert "v1.1"
    This reverts commit e08f3fdfbbf12454e9b1c1b27be8d7a9ff0d8c53.

--- frog.fs
+++ frog.fs
@@ -1,10 +1,9 @@
 #version 460 core
 in vec2 fs_tex;
 out vec4 Frag_color;
-uniform sampler2D texture1;
-//uniform vec4 color;
+uniform sampler2D texture;
 void main()
 {
-	Frag_color = texture(texture1,fs_tex);
-//	Frag_color = vec4(0.0f,0.0f,0.4f,1.0f);
+//	Frag_color = texture(texture,fs_tex);
+	Frag_color = vec4(0.0f,0.0f,0.4f,1.0f);
 }
